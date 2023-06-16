5b549c4dfef2bcf88dff53bccd98a8a0bc7e11f8
AbdulrahmanLuqman <fal528@dervac.com>
2023-06-16 14:33:28 +0100

    done with nav and footer

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
-<script setup>
+<script>
+import { defineComponent } from 'vue';
+import Nav from './components/Navbar.vue'
+import Footer from './components/Footer.vue';
 
+export default defineComponent({
+  components: {Nav, Footer},
+  data(){
+    return{
+    }
+  }
+})
 </script>
 
 <template>
-
+  <Nav class="" />
+  <Footer class="z-[0]" />
 </template>
 
 <style scoped>
--- src/stores/counter.js
+++ src/stores/counter.js
@@ -3,7 +3,9 @@ import { defineStore } from 'pinia'
 export default function Store(){
   return defineStore('elements', {
     state: () =>({
-
+      pagesToggle: false,
+      servicesToggle: false,
+      showMobileNav: false,
     })
   })()
 }
