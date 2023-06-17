13426d86b6abac2d37559e20ae824b5a2e0e1d78
AbdulrahmanLuqman <fal528@dervac.com>
2023-06-17 10:14:59 +0100

    made some corrections

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -16,7 +16,7 @@ export default defineComponent({
 
 <template>
     <header class="sticky top-0 z-[999] shadow-xl">
-      <div class="lg:bg-[#1475C4] lg:h-[40px] lg:w-full lg:flex lg:items-center lg:justify-between lg:px-[80px] hidden">
+      <div class="lg:bg-[#1475C4] lg:h-[40px] lg:w-full lg:flex lg:items-center lg:justify-between lg:px-12 hidden">
         <div class="flex items-center gap-10">
           <div class="flex items-center gap-2">
             <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24"><path fill="white" d="M2 20V4h20v16H2Zm10-7l8-5V6l-8 5l-8-5v2l8 5Z"/></svg>
@@ -41,7 +41,7 @@ export default defineComponent({
           <p><a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24"><path fill="white" d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4H7.6m9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8A1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5a5 5 0 0 1-5 5a5 5 0 0 1-5-5a5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3a3 3 0 0 0 3 3a3 3 0 0 0 3-3a3 3 0 0 0-3-3Z"/></svg></a></p>
         </div>
       </div>
-      <nav class="px-[80px] bg-[#F9FBFE] h-[80px] w-full flex justify-between items-center cursor-pointer">
+      <nav class="md:px-12 px-4 bg-[#F9FBFE] h-[80px] w-full flex justify-between items-center cursor-pointer">
         <div>
           <img width="40" class="z-[999]" src="../assets/images/logo.png" alt="LOGO">
         </div>
