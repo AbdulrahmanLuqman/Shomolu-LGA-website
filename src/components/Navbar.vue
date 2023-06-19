7ceea6c91dfc5528e76ea6de8fd0c38c15758e21
AbdulrahmanLuqman <fal528@dervac.com>
2023-06-19 10:14:28 +0100

    changed some section positions

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -32,7 +32,7 @@ export default defineComponent({
           </div>
           <div class="flex items-center gap-2">
             <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24"><path fill="white" d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5a2.5 2.5 0 0 1 0-5a2.5 2.5 0 0 1 0 5z"/></svg>
-            <p class="text-white text-sm">22 Town Planning Way, Ilupeju, Mushin</p>
+            <p class="text-white text-sm">Somolu Local Government Bashua Complex, Somolu</p>
           </div>
         </div>
   
