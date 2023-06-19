e05c42b95a3fba5c44e767e04c63cd123db37586
AbdulrahmanLuqman <fal528@dervac.com>
2023-06-19 07:21:12 +0100

    addedd animations

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -18,9 +18,13 @@ export default defineComponent({
         <img width="170" src="../assets/images/header-shape-1.png" class="bounce z-0 absolute top-0 left-8" alt="">
         <img width="170" src="../assets/images/header-shape-2.png" class="bounce z-0 absolute bottom-0 right-8" alt="">
         <div class="grid md:grid-cols-2 gap-10 items-center h-full md:py-0 py-12 md:px-12 px-4">
-            <div class="text-[whitesmoke] flex flex-col gap-8 items-center md:items-start">
+            <div class="text-[whitesmoke] flex flex-col gap-8 items-center md:items-start overflow-hidden">
                 <p class="text-sm">Discover the city</p>
-                <p class="lg:text-7xl md:text-start md:text-4xl text:3xl font-bold text-center">Cooperation has become a central element</p>
+                <div class="flex justify-between w-[300%] overflow-hidden slide">
+                    <p class="w-full lg:text-5xl md:text-start md:text-4xl text:3xl font-bold text-center">Cooperation has become a central element</p>
+                    <p class="w-full lg:text-5xl md:text-start md:text-4xl text:3xl font-bold text-center">We developing a happy and sustainable city</p>
+                    <p class="w-full lg:text-5xl md:text-start md:text-4xl text:3xl font-bold text-center">Our people are our top priority</p>
+                </div>
                 <p class="bg-[#F9FBFE] w-fit px-6 py-2 text-[#1475C4] rounded-lg font-semibold">Learn more</p>
             </div> 
             <div>
@@ -148,5 +152,41 @@ export default defineComponent({
 }
 .services:hover p{
     color: whitesmoke;
+}
+.slide{
+    animation: slide 15s infinite alternate ease-in-out 3s;
+    position: relative;
+}
+@keyframes slide{
+    10%{
+        left: 0;
+    }
+    20%{
+        left: 0;
+    }
+    30%{
+        left: 0;
+    }
+    40%{
+        left: -100%;
+    }
+    50%{
+        left: -100%;
+    }
+    60%{
+        left: -100%;
+    }
+    70%{
+        left: -100%;
+    }
+    80%{
+        left: -200%;
+    }
+    90%{
+        left: -200%;
+    }
+    100%{
+        left: -200%;
+    }
 }
 </style>
